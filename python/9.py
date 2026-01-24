d30ee6136b39715879e5c226dfd60234b66334c2
manvivarshney567-prog <manvivarshney567@gmail.com>
2026-01-24 22:13:39 +0530

    ques9

--- python/9.py
+++ python/9.py
@@ -11,3 +11,29 @@
 # 3       3-7
 # 4       2-8
 # 5       1-9
+# true=*
+# false=space
+
+
+# row      col>=5-row+1       col<=5+row-1
+# 1          5                   5
+# 2          4                   6
+# 3          3                   7
+# 4          2                   8
+# 5          1                   9
+
+
+
+for row in range(1,6):
+    for col in range(1,10):
+        if(col>=5-row+1 and col<=5+row-1):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print( )
+
+
+
+
+
+
