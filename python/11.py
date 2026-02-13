348c6a53fcd983035326c4bc8158daa07802df77
manvivarshney567-prog <manvivarshney567@gmail.com>
2026-02-13 23:57:52 +0530

    updated file

--- python/11.py
+++ python/11.py
@@ -19,13 +19,27 @@
 # 4             2                8
 # 5             1                9
 
-for  row in range(1,6):
+# for  row in range(1,6):
+#     for col in range(1,10):
+#         if(col<5-row+1 or col>5+row-1):
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
+for row in range(1,6):
     for col in range(1,10):
-        if(col<5-row+1 or col>5+row-1):
-            print("*",end=" ")
+        if col>=5-row+1 and col<=5+row-1:
+            if(row+col)%2==0:
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
         else:
             print(" ",end=" ")
     print()
+
+
+
 
 
 
